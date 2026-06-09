8595ba44b7b6f9c1452e66ab78d4b3e42c6707fa
Юлия <julia.dolgov.07@mail.ru>
2026-06-09 14:51:43 +0300

    Add base path and GitHub Actions workflow

--- PP_5_2026/vite.config.js
+++ PP_5_2026/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/PP_2026_05/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
